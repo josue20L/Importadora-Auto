d94bf52987d6467f4da86cfde60c8152ceed1c7c
josue20L <202005725@est.umss.edu>
2026-02-19 06:55:22 -0400

    corregido

--- js/login.js
+++ js/login.js
@@ -1,22 +1,43 @@
 function login() {
+
   const u = document.getElementById("user").value;
+
   const p = document.getElementById("pass").value;
 
+
+
   const encontrado = usuarios.find(
+
     x => x.user === u && x.pass === p
+
   );
 
+
+
   if (!encontrado) {
+
     document.getElementById("error").innerText = "Credenciales incorrectas";
+
     return;
+
   }
+
+
 
   sessionStorage.setItem("rol", encontrado.rol);
+
   sessionStorage.setItem("usuario", encontrado.user);
 
+
+
   if (encontrado.rol === "ADMIN") {
+
     window.location.href = "admin.html";
+
   } else {
+
     window.location.href = "usuario.html";
+
   }
+
 }
